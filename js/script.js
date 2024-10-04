3dffafd6a884322cb209ec532d26c5f8d4b5a4f3
kareematef01 <karimatefpro@gmail.com>
2024-10-04 20:18:34 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 window.addEventListener('scroll', function() {
     let navbar = document.getElementById('navbar'); 
-    var logo = document.getElementById('logo');
+    
     if (window.scrollY > 50) { 
       navbar.classList.add('navbar-scrolled');
-      logo.src = '../images/logo-dark.png'; 
     } else {
       navbar.classList.remove('navbar-scrolled'); 
-      logo.src = '../images/logo-light.png';
+      
     }
   });
 
-
+  window.addEventListener('scroll', function() {
+    var logo = document.getElementById('logo');
+    if (window.scrollY > 50) { 
+        navbar.classList.add('navbar-scrolled');
+        console.log('Changing to dark logo');
+        logo.src = 'images/logo-dark.png'; // تأكد من صحة المسار
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+        console.log('Changing to light logo');
+        logo.src = 'images/logo-light.png'; // تأكد من صحة المسار
+    }
+});
+  
   
